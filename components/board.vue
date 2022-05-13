5766b3865e364af153a4e33cd1931206924ac567
Hadrien Combaz <hcombaz@gmail.com>
2022-05-13 23:07:41 +0200

    master : add component board but broken

---
+++ components/board.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="board">
+        <div v-for="x in 3" :key="x" class="row">
+            <div v-for="y in 3" :key="y" class="square" v-on:click="place(x,y)">
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                Board :[
+                    ['','',''],
+                    ['','',''],
+                    ['','','']]
+            }
+        },
+        methods: {
+            getboard(x,y){
+                return { Board[x][y] }
+            },
+            place(x,y){
+                this.Board[x][y] = 'X'
+            },
+        }
+    }
+
+</script>
+
+<style scoped>
+.board {
+    margin: 50px;
+    display: grid;
+    grid-column: 3;
+    grid-row: 3;
+}
+.square {
+  background: #fff;
+  border: 1px solid #999;
+  float: left;
+  font-size: 70px;
+  font-weight: bold;
+  line-height: 34px;
+  height: 100px;
+  margin-right: -1px;
+  margin-top: -1px;
+  padding: 0;
+  text-align: center;
+  width: 100px;
+}
+</style>
--- pages/local.vue
+++ pages/local.vue
@@ -1,8 +1,17 @@
 <template>
     <div>
-        <h1>LOCAL</h1>
-        <nuxt-link to="/">
-            <button>Home</button>
-        </nuxt-link>
+        <div>
+            <h1>LOCAL</h1>
+            <nuxt-link to="/">
+                <button>Home</button>
+            </nuxt-link>
+        </div>
+        <Board />
     </div>
 </template>
+
+<script>
+import Board from "../components/board.vue";
+
+export default { components: { Board } }
+</script>
