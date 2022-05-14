73e00d0b156a154ce7c9c89bc5b7bbcee5c0d02b
Thomas Witz <thomas.witz@epita.fr>
2022-05-14 02:47:07 +0000

    add default layer (with footer) and a 404 error page

---
+++ layouts/default.vue
@@ -0,0 +1,88 @@
+<template>
+  <div>
+      <Nuxt />
+      <Footer />
+  </div>
+</template>
+
+<script>
+import Footer from '~/components/Footer.vue';
+
+export default {
+	components: {
+		Footer
+	}
+}
+</script>
+
+<style>
+html {
+  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+body {
+  background-color: #212121;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  width: 100%;
+  text-align: center;
+  margin: 10% 0;
+  min-height: 200px;
+  align-items: center;
+}
+
+.title {
+  font-size: xx-large;
+  color: #E55E5B;
+  margin-bottom: 0;
+}
+
+.title p {
+  font-size: large;
+  color: white;
+  margin-top: 0;
+}
+
+.button {
+  padding: 10px 20px;
+  border-radius: 5px;
+  border: 1px solid white;
+  border-radius: 5px;
+  text-transform: uppercase;
+  cursor: pointer;
+  transition: all .2s;
+  background-color: #E55E5B;
+  color:white;
+  font-weight: 500;
+  text-decoration: none;
+  font-size: x-large;
+
+  flex: 1 auto;
+  justify-content: space-around;
+}
+
+.button:hover {
+  padding: 15px 25px;
+  color: #E55E5B;
+  background-color: white;
+  border: 1px solid #E55E5B;
+}
+</style>
---
+++ layouts/error.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <h1 v-if="error.statusCode === 404">404 Page not found</h1>
+    <h1 v-else>An error occurred</h1>
+    <div>
+      <NuxtLink to="/">Home page</NuxtLink>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['error'],
+  layout: 'blog' // you can set a custom layout for the error page
+}
+</script>
+
+<style scoped>
+.container {
+	margin: 0 auto;
+	height: 80vh;
+	display: flex;
+	padding-top: 60px;
+}
+</style>
